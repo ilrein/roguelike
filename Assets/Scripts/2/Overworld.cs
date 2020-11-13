39296b61e0adde9548f01e0f0b55254115bf88e1
merLO <ilia.reingold@gmail.com>
2020-11-12 22:48:32 -0500

    checkpoint on node spacing

--- Assets/Scripts/2/Overworld.cs
+++ Assets/Scripts/2/Overworld.cs
@@ -25,9 +25,34 @@ public class Overworld : MonoBehaviour
   private void GenerateBlock()
   {
     float amount = Mathf.Round(Random.Range(0f, 4f));
-    for (int i = 0; i < amount; i++)
+
+    if (amount == 0) amount = 1;
+
+    var height = node1.GetComponent<RectTransform>().rect.height;
+    var breakpoint = 0f;
+
+    //
+    // if 1 breakpoint = 50% 1/2
+    // 2 breakpoint = 33% 1/3
+    // 3 breakpoint 25% 1/4
+    // 4 breakpoint 20% 1/5
+    //
+
+    if (amount == 1) breakpoint = height / 2;
+    if (amount == 2) breakpoint = height / 3;
+    if (amount == 3) breakpoint = height / 4;
+    if (amount == 4) breakpoint = height / 5;
+
+    for (int i = 1; i < amount + 1; i++)
     {
-      Instantiate(encounterPrefab, node1.transform);
+      Debug.Log(amount);
+
+      Instantiate(
+        encounterPrefab,
+        new Vector3(node1.transform.position.x, breakpoint * i),
+        node1.transform.rotation,
+        node1.transform
+      );
     }
   }
 
